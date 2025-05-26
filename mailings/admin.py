6f5250925be1ec68212d0ac2582370924f37c41c
Svyatoslav <arend359@gmail.com>
2025-05-26 23:08:11 +0300

    Register models in admin and enable superuser login

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Client, Message, Mailing, MailingAttempt
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("email", "full_name", "comment")
+    search_fields = ("email", "full_name")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("subject",)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("start_time", "end_time", "status")
+    list_filter = ("status",)
+    search_fields = ("message__subject",)
+    filter_horizontal = ("clients",)
+
+
+@admin.register(MailingAttempt)
+class MailingAttemptAdmin(admin.ModelAdmin):
+    list_display = ("attempt_time", "status", "mailing")
+    list_filter = ("status",)
+    search_fields = ("server_response",)
