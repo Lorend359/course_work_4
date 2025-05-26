48367baa2c523a2ee1637e249c2935ff8d2b6f0d
Svyatoslav <arend359@gmail.com>
2025-05-26 23:23:29 +0300

    Add Client delete view and confirmation template

--- mailings/urls.py
+++ mailings/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("clients/", views.ClientListView.as_view(), name="client_list"),
     path("clients/create/", views.ClientCreateView.as_view(), name="client_create"),
     path("clients/<int:pk>/update/", views.ClientUpdateView.as_view(), name="client_update"),
+    path("clients/<int:pk>/delete/", views.ClientDeleteView.as_view(), name="client_delete"),
+
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import Client
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, DeleteView
+
 
 
 class ClientListView(ListView):
@@ -21,4 +22,10 @@ class ClientUpdateView(UpdateView):
     model = Client
     template_name = "mailings/client_form.html"  # используем тот же шаблон
     fields = ["email", "full_name", "comment"]
+    success_url = reverse_lazy("mailings:client_list")
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = "mailings/client_confirm_delete.html"
     success_url = reverse_lazy("mailings:client_list")
