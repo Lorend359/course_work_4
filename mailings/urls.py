f0c4fd904c82569a70b9dea6ff82cb0b03f6eba6
Svyatoslav <arend359@gmail.com>
2025-05-26 23:20:37 +0300

    Add Client update view and update link

--- mailings/urls.py
+++ mailings/urls.py
@@ -6,4 +6,5 @@ app_name = "mailings"
 urlpatterns = [
     path("clients/", views.ClientListView.as_view(), name="client_list"),
     path("clients/create/", views.ClientCreateView.as_view(), name="client_create"),
+    path("clients/<int:pk>/update/", views.ClientUpdateView.as_view(), name="client_update"),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import Client
+from django.views.generic import UpdateView
 
 
 class ClientListView(ListView):
@@ -12,5 +13,12 @@ class ClientListView(ListView):
 class ClientCreateView(CreateView):
     model = Client
     template_name = "mailings/client_form.html"
+    fields = ["email", "full_name", "comment"]
+    success_url = reverse_lazy("mailings:client_list")
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    template_name = "mailings/client_form.html"  # используем тот же шаблон
     fields = ["email", "full_name", "comment"]
     success_url = reverse_lazy("mailings:client_list")
